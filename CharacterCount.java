903f343e272cae5d55df253500f242fa7f159673
siddi-Harshini <154400547+siddi-Harshini@users.noreply.github.com>
2024-08-15 11:26:42 +0530

    Add files via upload

---
+++ CharacterCount.java
@@ -0,0 +1,7 @@
+public class CharacterCount {
+    public static void main(String[] args) {
+        String text = "Hello";
+        int count = text.length();
+        System.out.println("Total number of characters: " + count);
+    }
+}
